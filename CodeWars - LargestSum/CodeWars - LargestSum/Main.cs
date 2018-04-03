520965ed97e0e89470c570f767076127a30a7d5c
kevinlimantoro <vinlimantoro@gmail.com>
2018-04-03 15:49:45 +0800

    Add project files.

---
+++ CodeWars - LargestSum/CodeWars - LargestSum/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CodeWars
+{
+    public static class Kata
+    {
+        public static int LargestSum(int[] arr)
+        {
+            var res = 0;
+            var maxres = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res += arr[i];
+                if(res < 0)
+                {
+                    res = 0;
+                }
+                if (maxres < res)
+                {
+                    maxres = res;
+                }
+            }
+            return maxres;
+        }
+    }
+}
---
+++ CodeWars - LargestSum/UnitTestProject1/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using NUnit.Framework;
+
+namespace LargestSumTest
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [TestCase(new[] { -1, -2, -3 }, ExpectedResult = 0)]
+        [TestCase(new int[0], ExpectedResult = 0)]
+        [TestCase(new[] { 1, 2, 3, 4 }, ExpectedResult = 10)]
+        [TestCase(new[] { 31, -41, 59, 26, -53, 58, 97, -93, -23, 84 }, ExpectedResult = 187)]
+        [TestCase(new[] { 31, -41, 59, 26, -53, 58, 97, -1, -23, 84 }, ExpectedResult = 247)]
+        public int MultipleTest(int[] input)
+        {
+            return Kata.LargestSum(input);
+        }
+    }
+}
